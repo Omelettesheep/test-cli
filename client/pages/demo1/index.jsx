6b02865108c6e24709f3d30fe39e309ca213e2a1
yangyi20 <yangyi20@baidu.com>
2019-07-01 11:21:49 +0800

    添加react-redux配置项

--- client/pages/demo1/index.jsx
+++ client/pages/demo1/index.jsx
@@ -3,22 +3,20 @@ import { Button } from 'antd';
 import request from '@utils/request';
 import { connect } from 'react-redux'
 
-// export default class Demo1 extends React.Component {
 class Demo1 extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
     componentDidMount() {
-        console.log(`当前时间 ${Date.now()}: yangyi20最美 debug 的数据是 this.props: `, this.props)
         // 试试本地Mock
         request.get('/mock/user.json').then(res=> {
-            console.log(`当前时间 ${Date.now()}: yangyi20最美 debug 的数据是 res: `, res)
+            console.log('res: ', res);
         });
 
         // 试试easy mock
         request.get('https://www.easy-mock.com/mock/5d14a54e94d3053851e1023b/v1/userInfo').then(res => {
-            console.log(`当前时间 ${Date.now()}: yangyi20最美 debug 的数据是 res: `, res)
+            console.log('res: ', res)
         })
     }
 
